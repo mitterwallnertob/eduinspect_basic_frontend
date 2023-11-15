dc6fe8936cf18f3fd35cbf7e548a86f3e9df4624
Tobias Mitterwallner <tobias.mitterwallner@htl-wels.at>
2023-11-15 13:39:35 +0100

    fixed lesson.jsx

--- components/timetable/lesson.jsx
+++ components/timetable/lesson.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Lesson({ lessonData }) {
   return (
-    <div className="grid grid-cols-2 gap-y-4 gap-x-20 mb-2 border-b border-gray-200 p-2">
+    <div className="grid grid-cols-2 gap-y-4 gap-x-[4.5rem] mb-2 border-b border-gray-200 p-2">
       <div className="font-medium text-gray-800">{lessonData.klasse}</div>
       <div className="text-gray-600">{lessonData.teacher}</div>
       <div className="text-gray-600">{lessonData.subject}</div>
